564b3af0525d1e811067f0237395965771bb6e20
Francisco Gonzalez <francisco.gonzalez.bustamante@gmail.com>
2016-10-11 19:33:50 -0700

    Avance hoy

--- leerTXT.groovy
+++ leerTXT.groovy
@@ -1,21 +1,34 @@
 class LeerArchivo{
 
 	static LeerArchivoTexto(String nombre){
-		def expresionTipoDato = "/bool|byte|char|double|float|int|long|short|string|date/g";
-		def expresionCondicionBooleana = "/true|false/g"
-		def i=0
+		def expresionTipoDato = "/(bool|byte|char|double|float|int|long|short|string|date)/g";
+		def expresionOperadorCondicional = "/(true|false)/g"
+		def expresionCiclo = "/(for|while|dowhile|loop|foreach)/g"
+		def expresionCondicion = "/(if|else|try|catch|switch|case|break|default)/g" 
+		def expresionOperadorComparacion = "/(==|<=|>=)/g"
+
+		def i=1
 		new File(nombre).eachLine { line ->
 			if(line != ""){
 				def texto = line.substring(0).split(",");	
 				if (texto != null){
 					if (expresionTipoDato =~ texto[0]) {
-						println(i + "- " + texto[0] + "->" + "Tipo de dato");
+						println(i + "- " + texto[0] + " -> " + "Tipo de dato");
 					}	
-					else if(expresionCondicionBooleana =~ texto[0]){
-						println(i + "- " + texto[0] + "->" + "Codicion booleana");
+					else if(expresionOperadorCondicional =~ texto[0]){
+						println(i + "- " + texto[0] + " -> " + "Operador Condicional");
+					}
+					else if (expresionCiclo =~ texto[0]) {
+						println(i + "- " + texto[0] + " -> " + "Ciclo");
+					}
+					else if (expresionCondicion =~ texto[0]) {
+						println(i + "- " + texto[0] + " -> " + "Condicional");
+					}
+					else if (expresionOperadorComparacion =~ texto[0]) {
+						println(i + "- " + texto[0] + " -> " + "Operador de comparacion");
 					}
 					else{
-						println(i + "- " + texto[0] + "->desconocido");
+						println(i + "- " + texto[0] + " -> desconocido");
 					}
 					i++;
 				}
@@ -24,6 +37,6 @@ class LeerArchivo{
 	}
 
 	static void main(String[] args) {
-		LeerArchivoTexto("/home/frank/lexico.txt"); 
+		LeerArchivoTexto("lexico.txt"); 
 	}
 }
