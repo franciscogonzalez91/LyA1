956eab2171da94c65301c1555a8425ba850f80be
Francisco Gonzalez <francisco.gonzalez.bustamante@gmail.com>
2016-10-13 23:49:39 -0700

    Se avanzo la mayoria, ya estan 101 palabras en el txt, pero falta que reconozca alrededor de 40

--- leerTXT.groovy
+++ leerTXT.groovy
@@ -1,7 +1,7 @@
 class LeerArchivo{
 
 	static LeerArchivoTexto(String nombre){
-		def expresionTipoDato = "/bool|byte|char|double|float|int|long|short|string|date|object|var/g";
+		def expresionTipoDato = "/bool|byte|char|double|float|int|long|short|string|date|object|var|datetime|ToString|ToInt|ToBool|ToChar|ToDouble|ToFloat|ToLong|ToShort|ToLongDateString|ToShortDateString|BigInteger|BigDecimal/g";
 		def expresionOperadorCondicional = "/true|false/g"
 		def expresionCiclo = "/(for|while|dowhile|loop|foreach)/g"
 		def expresionCondicion = "/(if|else|try|catch|switch|case|break|default)/g" 
@@ -22,58 +22,59 @@ class LeerArchivo{
 		def i=1
 		new File(nombre).eachLine { line ->
 			if(line != ""){
-				def texto = line.substring(0).split(" ");	
+				def texto = line.toString()
+				//def texto = line.substring(0).split(" ");	
 				if (line != null){
-					if (expresionTipoDato =~ line) {
-						println(i + "- " + texto[0] + " -> " + "Tipo de dato");
+					if (expresionTipoDato =~ texto) {
+						println(i + "- " + texto + " -> " + "Tipo de dato");
 					}	
-					else if(expresionOperadorCondicional =~ texto[0]){
-						println(i + "- " + texto[0] + " -> " + "Operador Condicional");
+					else if(expresionOperadorCondicional =~ texto){
+						println(i + "- " + texto + " -> " + "Operador Condicional");
 					}
-					else if (expresionCiclo =~ texto[0]) {
-						println(i + "- " + texto[0] + " -> " + "Ciclo");
+					else if (expresionCiclo =~ texto) {
+						println(i + "- " + texto + " -> " + "Ciclo");
 					}
-					else if (expresionCondicion =~ texto[0]) {
-						println(i + "- " + texto[0] + " -> " + "Condicional");
+					else if (expresionCondicion =~ texto) {
+						println(i + "- " + texto + " -> " + "Condicional");
 					}
-					else if (expresionClase =~ texto[0]) {
-						println(i + "- " + texto[0] + " -> " + "Clase");
+					else if (expresionClase =~ texto) {
+						println(i + "- " + texto + " -> " + "Clase");
 					}
-					else if (expresionModificadorProteccion =~ texto[0]) {
-						println(i + "- " + texto[0] + " -> " + "Modificador de proteccion");					
+					else if (expresionModificadorProteccion =~ texto) {
+						println(i + "- " + texto + " -> " + "Modificador de proteccion");					
 					}
-					else if (expresionIdentificador =~ texto[0]) {
-						println(i + "- " + texto[0] + " -> " + "Identificador");
+					else if (expresionIdentificador =~ texto) {
+						println(i + "- " + texto + " -> " + "Identificador");
 					}
-					else if (expresionMetodo =~ texto[0]) {
-						println(i + "- " + texto[0] + " -> " + "Metodo");
+					else if (expresionMetodo =~ texto) {
+						println(i + "- " + texto + " -> " + "Metodo");
 					}
-					else if (expresionOperadorAsignacion =~ texto[0]) {
-						println(i + "- " + texto[0] + " -> " + "Operador de Asignacion");
+					else if (expresionOperadorAsignacion =~ texto) {
+						println(i + "- " + texto + " -> " + "Operador de Asignacion");
 					}
-					else if (expresionOperadorRacional =~ texto[0]) {
-						println(i + "- " + texto[0] + " -> " + "Operador Racional");
+					else if (expresionOperadorRacional =~ texto) {
+						println(i + "- " + texto + " -> " + "Operador Racional");
 					}
-					else if (expresionImpresor =~ texto[0]) {
-						println(i + "- " + texto[0] + " -> " + "Impresor");
+					else if (expresionImpresor =~ texto) {
+						println(i + "- " + texto + " -> " + "Impresor");
 					}
-					else if (expresionFuncion =~ texto[0]) {
-						println(i + "- " + texto[0] + " -> " + "Funcion");
+					else if (expresionFuncion =~ texto) {
+						println(i + "- " + texto + " -> " + "Funcion");
 					}
-					/*else if (expresionObjeto =~ texto[0]) {
-						println(i + "- " + texto[0] + " -> " + "Objeto");
+					/*else if (expresionObjeto =~ texto) {
+						println(i + "- " + texto + " -> " + "Objeto");
 					}*/
-					else if (expresionAsignador =~ texto[0]) {
-						println(i + "- " + texto[0] + " -> " + "Asignador");
+					else if (expresionAsignador =~ texto) {
+						println(i + "- " + texto + " -> " + "Asignador");
 					}
-					else if (expresionRetorno =~ texto[0]) {
-						println(i + "- " + texto[0] + " -> " + "Retorno");
+					else if (expresionRetorno =~ texto) {
+						println(i + "- " + texto + " -> " + "Retorno");
 					}
-					else if (expresionNull =~ texto[0]) {
-						println(i + "- " + texto[0] + " -> " + "Nulo");
+					else if (expresionNull =~ texto) {
+						println(i + "- " + texto + " -> " + "Nulo");
 					}
 					else{
-						println(i + "- " + texto[0] + " -> desconocido");
+						println(i + "- " + texto + " -> desconocido");
 					}
 					i++;
 				}
