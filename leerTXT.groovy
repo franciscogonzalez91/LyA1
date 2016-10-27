e8f39b4f803d3fcd78806523938c7d3c293d2105
Francisco Gonzalez <francisco.gonzalez.bustamante@gmail.com>
2016-10-27 12:57:16 -0700

    encontre como reconocer lo que le pongo

--- leerTXT.groovy
+++ leerTXT.groovy
@@ -1,88 +1,76 @@
 class LeerArchivo{
+    static LeerArchivoTexto(String nombre){
+        def expresionTipoDato = "^(bool|byte|char|double|float|int|long|short|string|date|object|var|datetime|ToString|ToInt|ToBool|ToChar|ToDouble|ToFloat|ToLong|ToShort|ToLongDateString|ToShortDateString|BigInteger|BigDecimal)*";
+        def expresionOperadorCondicional = "^(true|false)*"
+        def expresionCiclo = "^(for|while|dowhile|loop|foreach)*"
+        def expresionCondicion = "^(if|else|try|catch|switch|case|break|default)*" 
+        def expresionClase = "^(class|random|convert|console)*"
+        def expresionModificadorProteccion = "^(public|protected|private)*"
+        def expresionIdentificador = ".|;"
+        def expresionMetodo = "^(main)*"
+        def expresionImpresor = "^(println)*"
+        def expresionOperadorAsignacion = "^(=)*"
+        def expresionOperadorRacional = "^(==|<=|>=)*"
+        def expresionOperadorMatematico = "^([-+/*])*"
+        def expresionFuncion = "^(void)*"
+        def expresionObjeto = "^(object)*"
+        def expresionAsignador = "^(set)*"
+        def expresionRetorno = "^(return)*"
+        def expresionNull = "^(null)*"
 
-	static LeerArchivoTexto(String nombre){
-		def expresionTipoDato = "/bool|byte|char|double|float|int|long|short|string|date|object|var|datetime|ToString|ToInt|ToBool|ToChar|ToDouble|ToFloat|ToLong|ToShort|ToLongDateString|ToShortDateString|BigInteger|BigDecimal/g";
-		def expresionOperadorCondicional = "/true|false/g"
-		def expresionCiclo = "/(for|while|dowhile|loop|foreach)/g"
-		def expresionCondicion = "/(if|else|try|catch|switch|case|break|default)/g" 
-		def expresionClase = "/(class|random|convert|console)/g"
-		def expresionModificadorProteccion = "/(public|protected|private)/g"
-		def expresionIdentificador = "/(;|.)/g"
-		def expresionMetodo = "/main/g"
-		def expresionImpresor = "/println/g"
-		def expresionOperadorAsignacion = "/(=)/g"
-		def expresionOperadorRacional = "/(==|<=|>=)/g"
-		def expresionFuncion = "/void/g"
-		//def expresionObjeto = "/object/g"
-		def expresionAsignador = "/set/g"
-		def expresionRetorno = "/return/g"
-		def expresionNull = "/null/g"
 
-
-		def i=1
-		new File(nombre).eachLine { line ->
-			if(line != ""){
-				def texto = line.toString()
-				//def texto = line.substring(0).split(" ");	
-				if (line != null){
-					if (expresionTipoDato =~ texto) {
-						println(i + "- " + texto + " -> " + "Tipo de dato");
-					}	
-					else if(expresionOperadorCondicional =~ texto){
-						println(i + "- " + texto + " -> " + "Operador Condicional");
-					}
-					else if (expresionCiclo =~ texto) {
-						println(i + "- " + texto + " -> " + "Ciclo");
-					}
-					else if (expresionCondicion =~ texto) {
-						println(i + "- " + texto + " -> " + "Condicional");
-					}
-					else if (expresionClase =~ texto) {
-						println(i + "- " + texto + " -> " + "Clase");
-					}
-					else if (expresionModificadorProteccion =~ texto) {
-						println(i + "- " + texto + " -> " + "Modificador de proteccion");					
-					}
-					else if (expresionIdentificador =~ texto) {
-						println(i + "- " + texto + " -> " + "Identificador");
-					}
-					else if (expresionMetodo =~ texto) {
-						println(i + "- " + texto + " -> " + "Metodo");
-					}
-					else if (expresionOperadorAsignacion =~ texto) {
-						println(i + "- " + texto + " -> " + "Operador de Asignacion");
-					}
-					else if (expresionOperadorRacional =~ texto) {
-						println(i + "- " + texto + " -> " + "Operador Racional");
-					}
-					else if (expresionImpresor =~ texto) {
-						println(i + "- " + texto + " -> " + "Impresor");
-					}
-					else if (expresionFuncion =~ texto) {
-						println(i + "- " + texto + " -> " + "Funcion");
-					}
-					/*else if (expresionObjeto =~ texto) {
-						println(i + "- " + texto + " -> " + "Objeto");
-					}*/
-					else if (expresionAsignador =~ texto) {
-						println(i + "- " + texto + " -> " + "Asignador");
-					}
-					else if (expresionRetorno =~ texto) {
-						println(i + "- " + texto + " -> " + "Retorno");
-					}
-					else if (expresionNull =~ texto) {
-						println(i + "- " + texto + " -> " + "Nulo");
-					}
-					else{
-						println(i + "- " + texto + " -> desconocido");
-					}
-					i++;
-				}
-			}					
-		}
-	}
-
-	static void main(String[] args) {
-		LeerArchivoTexto("lexico.txt"); 
-	}
+        def i=1
+        new File(nombre).eachLine { line ->
+            if(line != ""){
+                def texto = line.toString()
+                def input = texto.toString();
+                print i + " "
+                if (line != null){                    
+                    if(String.valueOf(input).matches(expresionTipoDato)){
+                        print input + " > " + "Tipo de Dato" + "\n"
+                    }
+                    else if(String.valueOf(input).matches(expresionOperadorCondicional)){
+                        print input + " > " + "Operador Condicional"  + "\n"
+                    }
+                    else if(String.valueOf(input).matches(expresionCiclo)){
+                        print input + " > " + "Ciclo"  + "\n"
+                    }
+                    else if(String.valueOf(input).matches(expresionCondicion)){
+                        print input + " > " + "Condicional"  + "\n"
+                    }
+                    else if(String.valueOf(input).matches(expresionClase)){
+                        print input + " > " + "Clase"  + "\n"
+                    }
+                    else if(String.valueOf(input).matches(expresionModificadorProteccion)){
+                        print input + " > " + "Modificador de Proteccion"  + "\n"
+                    }
+                    else if(String.valueOf(input).matches(expresionIdentificador)){
+                        print input + " > " + "Identificador"  + "\n"
+                    }
+                     else if(String.valueOf(input).matches(expresionMetodo)){
+                        print input + " > " + "Metodo"  + "\n"
+                    }
+                    else if(String.valueOf(input).matches(expresionImpresor)){
+                        print input + " > " + "Impresion"  + "\n"
+                    }
+                    else if(String.valueOf(input).matches(expresionOperadorAsignacion)){
+                        print input + " > " + "Operador de Asignacion"  + "\n"
+                    }
+                    else if(String.valueOf(input).matches(expresionOperadorRacional)){
+                        print input + " > " + "Operador Racional"  + "\n"
+                    }
+                    else if(String.valueOf(input).matches(expresionOperadorMatematico)){ //("[-+*/]")){
+                        print input + " > " + "Operador Aritmetico"  + "\n"
+                    }                    
+                    else{
+                        print input + " > " + "Error" + "\n";
+                    }
+                }
+            }
+            i++
+        }
+    }
+    static void main(String[] args) {
+        LeerArchivoTexto("C:\\Users\\Francisco\\Documents\\Repositorios\\LyA1\\LyA1\\lexico.txt"); 
+    }
 }
