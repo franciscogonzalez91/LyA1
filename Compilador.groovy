5b9c2004ff206ecfb02b2c23f463278cb6e74b50
Francisco Gonzalez <francisco.gonzalez.bustamante@gmail.com>
2016-11-13 12:47:07 -0800

    aun no entiendo porque fallan los regex

--- Compilador.groovy
+++ Compilador.groovy
@@ -4,27 +4,65 @@ import java.io.*;
 
 class Leer {    
     static Reconocer(String Archivo){
+        /*Inicio y Fin expresiones*/
+        def regInicio = "/^("
+        def regFinal = ")+\$/"
+
+        /*Espacios, tabuladores, etc*/
+        def regAsignador = "->"
+        def regEspacio = "\\s"
+        def regCorIzqSyntaxis = "\\["
+        def regCorDerSyntaxis = "\\]"
+        def regMinimoUno = "+"
+        def regOpcional = "*"
+        def regTipoDato = "byte|int|long|float|double|string|bool"
+        def regParIzq = "("
+        def regParDer = ")"
+        def regPuntoComa = ";"
+        /*Palabras Reservadas*/
+        def palabReservClase = "Clase"
+        def palabReservDef = "Def:"
+        def palabReservModificadoresProteccion = "Protec|Pub|Priv"
+
+        /*Ids*/
+        def regId = "[a-zA-Z]+[0-9]"
+        //def reg
+        /*Expresiones ya completas*/
+        def regSintaxisClase = regInicio + palabReservClase + regEspacio + regMinimoUno + regParIzq + palabReservModificadoresProteccion + regParDer + regMinimoUno + regEspacio + regMinimoUno + regParIzq + regId + regOpcional + regParDer +  regCorIzqSyntaxis + regParIzq + regParIzq + regTipoDato + regParDer + regMinimoUno + regEspacio + regMinimoUno + regParIzq + regId + regOpcional + regParDer + regParDer + regMinimoUno + regEspacio + regOpcional + regParIzq + regPuntoComa + regMinimoUno + regEspacio + regOpcional + regParIzq + regTipoDato + regParDer + regMinimoUno + regEspacio + regMinimoUno + regParIzq + regParIzq + regId + regOpcional + regParDer + regMinimoUno + regParDer + regParDer + regOpcional + regCorDerSyntaxis + regFinal;
+        def regSintaxisDef = regInicio + palabReservDef + regEspacio + regMinimoUno + regId + regOpcional + regFinal;
+        def regSintaxisAsignacion = regInicio + regId + regOpcional + regEspacio + regOpcional + regAsignador + regEspacio + regOpcional + regId + regOpcional + regFinal;
         //def regAsignacionString = "^(([a-zA-Z0-9]+ ?)+?\s*->\s*\"([a-zA-Z0-9]+ ?)\"+?)$"
         /*def regAsignacion = "/^(([a-zA-Z0-9]+ ?)+?\s*->\s*([a-zA-Z0-9]+ ?)+?)$/"
-        def regClase = "/^(Clase (Protec|Pub|Priv) ([a-zA-Z]+[0-9]*)\[\])$/"
+        def regClase = "/^(Clase\s+(Protec|Pub|Priv)+\s+([a-zA-Z]+[0-9]*)\[((byte|int|long|float|double|string|bool|)+\s+([a-zA-Z]+[0-9]*))+\s*(;+\s*(byte|int|long|float|double|string|bool)+\s+(([a-zA-Z]+[0-9]*)+))*\\])+$/"
         def regComparacion = "/^([a-zA-Z0-9]+\s*<->\s*[a-zA-Z0-9]+)$/"
         def redDefinicion = "/^(Def: [a-zA-Z]+[0-9]*)/$"
         def regFuncion = "Func"
-        def regIf = "/^(Sicumple \[(\s)*([a-zA-Z]+[0-9]*)\] Entonces)$/"
-        def regElseIf = "/^(Sino \[(\s)*([a-zA-Z]+[0-9]*)\] Entonces)$/"
+        def regIf = "/^(Sicumple \[(\s)*([a-zA-Z]+[0-9]*)\\] Entonces)$/"
+        def regElseIf = "/^(Sino \[(\s)*([a-zA-Z]+[0-9]*)\\] Entonces)$/"
         def regElse = "/^(Sinocumple)$/"
-        /*def regFinalizar = "Finzaliza"
-        //def regImprimir = "/^(Imprimir [a-zA-Z0-9]+)$/"*/
+        /*def regFinalizar = "Finzaliza"*/
+        //def regImprimir = "/^(Imprimir [a-zA-Z0-9]+)$/\"*/"
         //def regModProtec = "/^Priv$|^Protec$|^Pub$/"*/
         def regOper = "^Oper:\\s*(([a-zA-Z]+[0-9]*\\s*->)+\\s*(\\(*[a-zA-Z0-9]+\\s*[\\/\\-\\+\\*]\\s*[a-zA-Z0-9]+\\)*)*\\s*([\\/\\-\\+\\*]*)(\\s*\\(*[a-zA-Z0-9]+\\s*[\\/\\-\\+\\*]\\s*[a-zA-Z0-9]+\\)*))\$" 
+        //println("/^(Clase\\s+(Protec|Pub|Priv)+\\s+([a-zA-Z]+[0-9]*)\\[((byte|int|long|float|double|string|bool)+\\s+([a-zA-Z]+[0-9]*))+\\s*(;+\\s*(byte|int|long|float|double|string|bool)+\\s+(([a-zA-Z]+[0-9]*)+))*\\])+\$/")
+        //println(regSintaxisClase)
+        def regClass = "/^Clase\\s+(Protec|Pub|Priv)+\\s+([a-zA-Z]+[0-9]*)\\[((byte|int|long|float|double|string|bool)+\\s+([a-zA-Z]+[0-9]*))+\\s*(;+\\s*(byte|int|long|float|double|string|bool)+\\s+(([a-zA-Z]+[0-9]*)+))*\\]\$/"
         
+        println (regSintaxisAsignacion);
+
         new File(Archivo).eachLine {line ->
-            println(line);        
+            println(line);       
             if(line != null){
-                def texto = line.toString();
+                def texto = line.toString().trim();
                 if(line!=""){
-                    if(texto =~ regOper){
-                        println("Es operacion Matematica" + " -> " + texto)
+                    if(texto =~ regSintaxisDef){
+                        println("Es una definicion" + " -> " + texto)
+                    }                    
+                    else if(texto =~ regOper){
+                        println("Es una operacion aritmetica" + " -> " + texto)
+                    }
+                    else if(texto =~ regSintaxisAsignacion) {
+                        println("Es una asignacion" + " -> " + texto)
                     }
                     else{
                         println("Ninguno")
@@ -32,15 +70,6 @@ class Leer {
                 }
             }
         }
-        /*BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
-        def input = br.readLine()
-
-        if(input =~ regOper){
-            println("Es operacion Matematica" + " -> " + input)
-        }    
-        else{
-            println("Ninguno")
-        }*/
     }
 
     static void main(String[] args) {
